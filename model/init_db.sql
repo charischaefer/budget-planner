f39b1bc45f5fa9656841439427a3d5ca141ad05b
Charlotte Schaefer <chari.schaefer@gmail.com>
2023-10-19 21:32:03 +0200

    Add sample data to database

--- model/init_db.sql
+++ model/init_db.sql
@@ -36,3 +36,52 @@ CREATE TABLE `users`(
     `email` CHAR(255) NOT NULL,
     `image` CHAR(255) NOT NULL
 );
+
+--
+-- Sample Data
+--
+
+INSERT INTO `categories` (`name`)
+VALUES
+    ('Housing'),
+    ('Transportation'),
+    ('Entertainment'),
+    ('Food & Dining'),
+    ('Education'),
+    ('Travel'),
+    ('Shopping'),
+    ('Savings'),
+    ('Health & Fitness'),
+    ('Gifts & Donations'),
+    ('Utilities'),
+    ('Taxes'),
+    ('Insurance'),
+    ('Debt Repayment'),
+    ('Personal Care'),
+    ('Salary'),
+    ('Freelance'),
+    ('Business'),
+    ('Investments'),
+    ('Rental Income'),
+    ('Interest');
+
+INSERT INTO `transactions` (`amount`, `date`, `source`, `type`, `category_id`, `user_id`)
+VALUES
+    (50, '2023-10-15', 'Grocery Store', 'Expense', 1, 1),
+    (100, '2023-10-16', 'Electricity Bill', 'Expense', 2, 1),
+    (30, '2023-10-17', 'Movie Tickets', 'Expense', 3, 1),
+    (40, '2023-10-15', 'Gas Station', 'Expense', 4, 1),
+    (25, '2023-10-16', 'Pharmacy', 'Expense', 5, 1),
+    (75, '2023-10-17', 'Restaurant', 'Expense', 3, 2),
+    (120, '2023-10-15', 'Internet Bill', 'Expense', 2, 2),
+    (50, '2023-10-16', 'Public Transport', 'Expense', 4, 2),
+    (20, '2023-10-17', 'Health Clinic', 'Expense', 5, 2),
+    (60, '2023-10-15', 'Grocery Store', 'Expense', 1, 3),
+    (90, '2023-10-16', 'Gas Station', 'Expense', 4, 3),
+    (35, '2023-10-17', 'Movie Tickets', 'Expense', 3, 3);
+
+INSERT INTO `users` (`username`, `password`, `firstname`, `lastname`, `email`, `image`)
+VALUES
+    ('user1', 'password1', 'Sheldon', 'Cooper', 'sheldon.cooper@email.com', 'user1.jpg'),
+    ('user2', 'password2', 'Jane', 'Smith', 'jane.smith@email.com', 'user2.jpg'),
+    ('user3', 'password3', 'Robert', 'Johnson', 'robert.johnson@email.com', 'user3.jpg');
