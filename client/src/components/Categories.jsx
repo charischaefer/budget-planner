e454eb0e3de18993cc95f4df1e54c1bf634195d3
Pavithra <pavithra.veerappa@vuelio.com>
2023-10-30 18:16:18 +0000

    Categories and Transactions pages

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 export default function Categories() {
+
+    //Show a list of Categories when I load the page
+
+    //A state to store the list of Categories
+    const [ categories, setCategories ] = useState([]);
+    const handleChange = (event) => {
+        setCategories(event.target.value);
+    };
+
+    //A function to fetch the list of Categories
+    const getCategories = async() => {
+        const response = await fetch('/api/categories');
+        const data = await response.json();
+        console.log(data);
+        setCategories(data);
+    };
+
+    useEffect(() => {
+        getCategories();
+    }, []);
+
     return (
         <div>
-            <h1>Categories</h1>
+            <h1>Categories</h1>                      
+                       <select
+                        className="custom-select"
+                          id="inputGroupSelect01"
+                          onChange={handleChange}
+                          name="categoryId"
+                        >
+                          <option selected>Choose...</option>
+                          {categories.map((c) => (
+                            <option value={c.id}>{c.categoryName}</option>
+
+                          ))}
+                        </select>
         </div>
     );
 }
--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -5,6 +5,18 @@ export default function Transactions() {
     return (
         <div>
             <h1>Transactions</h1>
+            <table>
+                <tr>
+                    <th>id</th>
+                    <th>amount</th>
+                    <th>date</th>
+                    <th>source</th>
+                    <th>type</th>
+                    <th>category_id</th>
+                    <th>user_id</th>
+                </tr>
+            </table>
+
         </div>
     );
 }
