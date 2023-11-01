342278559f1b5eeed3344bcc8f97e49148623f82
Charlotte Schaefer <chari.schaefer@gmail.com>
2023-11-01 11:47:56 +0100

    Finish categories page

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { PieChart, Pie, Cell, Tooltip, Label } from "recharts";
+import CategoryOverview from "./CategoryOverview";
 
 export default function Categories() {
     const [categoryData, setCategoryData] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState(null);
 
     // Get category percentages
     const getCategoryPercentages = async (userId) => {
@@ -26,12 +28,28 @@ export default function Categories() {
 
     const COLORS = ['#E57373', '#81C784', '#64B5F6', '#FFD54F', '#FF8A65', '#F06292', '#4DB6AC', '#7986CB', '#A1887F', '#90A4AE', '#FFB74D', '#4DD0E1', '#FF80AB', '#B39DDB'];
 
+    // Define a function to handle the click event
+    const handleClick = (entry) => {
+      setSelectedCategory(entry.category_id);
+    };
+
+    useEffect(() => {
+    }, [selectedCategory]);
+
     return (
-        <div>
+        <div className="Categories" style={{ height: "750px" }}>
           <h1>Categories</h1>
           {categoryData && categoryData.length > 0 ? (
             <PieChart width={500} height={500}>
-              <Pie dataKey="percentage" data={categoryData} cx="50%" cy="50%" outerRadius={150} fill="#8884d8" nameKey="category">
+              <Pie
+                dataKey="percentage"
+                data={categoryData}
+                cx="50%" cy="50%"
+                outerRadius={200}
+                fill="#8884d8"
+                nameKey="category"
+                onClick={handleClick}
+              >
                 {categoryData.map((entry, index) => (
                   <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                 ))}
@@ -41,6 +59,10 @@ export default function Categories() {
             </PieChart>
           ) : (
             <p>Loading...</p>
+          )}
+
+          {selectedCategory !== null && (
+            <CategoryOverview categoryName={selectedCategory} />
           )}
         </div>
       );
---
+++ client/src/components/CategoryOverview.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./CategoryOverview.css";
+
+export default function CategoryOverview({ categoryName }) {
+  const [transactions, setTransactions] = useState([]);
+
+  const categoriesMap = {
+    0: '💲',
+    1: '🏡',
+    2: '🚗',
+    3: '🍕',
+    4: '💡',
+    5: '🛡️',
+    6: '🌡️',
+    7: '💰',
+    8: '💳',
+    9: '🎥',
+    10: '✈️',
+    11: '👶',
+    12: '💅',
+    13: '🐾',
+    14: '🧩'
+  };
+
+  // Format the date
+  const formatDate = (dateStr) => {
+    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+    return new Date(dateStr).toLocaleDateString("en-US", options);
+  };
+
+  // Get transactions of selected category
+  useEffect(() => {
+    const getTransactions = async (userId) => {
+      try {
+        const { data } = await axios(`/api/transactions-by-category/${categoryName}`, {
+          headers: {
+            authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        });
+
+        // Log the transactions to check the data
+        console.log("Transactions data:", data);
+
+        setTransactions(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getTransactions();
+  }, [categoryName]);
+
+  return (
+    <div className="CategoryOverview">
+      <table>
+        <thead>
+          <tr>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>Source</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>
+                <span
+                  style={{
+                    color: transaction.type === "Income" ? "green" : "red",
+                  }}
+                >
+                  ${transaction.amount.toFixed(2)}
+                </span>
+              </td>
+              <td>{formatDate(transaction.date)}</td>
+              <td>{transaction.source}</td>
+              <td>{categoriesMap[transaction.category_id]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
