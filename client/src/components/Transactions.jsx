e56a0c7de6b7360ea6b71ec45fe8b5ded571f9a8
Charlotte Schaefer <chari.schaefer@gmail.com>
2023-11-06 19:49:20 +0100

    Add h1

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -5,6 +5,7 @@ export default function Transactions() {
     return (
         <div>
             <h1>Transactions</h1>
+            <h1>Transactions</h1>
             <NewTransaction />
         </div>
     );
